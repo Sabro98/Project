b5c4183b30805e4d70303576fbbf8c971b34114d
Sabro98 <inhyuk04@gmail.com>
2020-07-31 05:16:01 +0900

    stone move add

--- FlickingStone/FlickingStone/src/Flicking/GamePanel.java
+++ FlickingStone/FlickingStone/src/Flicking/GamePanel.java
@@ -9,39 +9,37 @@ public class GamePanel extends JPanel {
 	private final String BACKGROUND_COL = "#E1D385";
 	private final int WIDTH = 500;
 	private final int HEIGHT = 500;
-	private final Point WHITE_POINT = new Point(250, 95);
-	private final Point BLACK_POINT = new Point(250, 405);
+	private final String BLACK_STONE_PATH = "images/Blackstone.png";
+	private final String WHITE_STONE_PATH = "images/WhiteStone.png";
+	private final Point WHITE_INIT_POINT = new Point(250, 95);
+	private final Point BLACK_INIT_POINT = new Point(250, 405);
 	private Stone blackStone;
 	private Stone whiteStone;
 
 	GamePanel() {
 		setSize(WIDTH, HEIGHT);
 		setBackground(Color.decode(BACKGROUND_COL));
-		this.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.print("x : " + e.getX() + ", ");
-				System.out.println("y : " + e.getY());
-			}
-		});
 		setLayout(null);
 		setStones();
+//		this.addMouseListener(new MouseAdapter() {
+//			@Override
+//			public void mouseClicked(MouseEvent e) {
+//				System.out.print("x : " + e.getX() + ", ");
+//				System.out.println("y : " + e.getY());
+//			}
+//		});
 	}
-	
-	private void setStones() {
-		blackStone = new Stone("images/BlackStone.png");
-		blackStone.setBounds(BLACK_POINT.x - blackStone.getWidth()/2, BLACK_POINT.y - blackStone.getHeight()/2,
-				blackStone.getWidth(), blackStone.getHeight());
+
+	private void setStones() { // set stones information
+		blackStone = new Stone(BLACK_STONE_PATH, BLACK_INIT_POINT, "up");
+		whiteStone = new Stone(WHITE_STONE_PATH, WHITE_INIT_POINT, "down");
 		add(blackStone);
-		
-		whiteStone = new Stone("images/WhiteStone.png");
-		whiteStone.setBounds(WHITE_POINT.x - whiteStone.getWidth()/2, WHITE_POINT.y - whiteStone.getHeight()/2
-				, whiteStone.getWidth(), whiteStone.getHeight());
 		add(whiteStone);
-		
+
 	}
+
 	@Override
-	public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) { // draw black line in boards
 		super.paintComponent(g);
 		for (int x = 50, y = 0; x < getWidth(); x += 50) {
 			g.drawLine(x, y, x, y + getHeight());
--- FlickingStone/FlickingStone/src/Flicking/Stone.java
+++ FlickingStone/FlickingStone/src/Flicking/Stone.java
@@ -1,13 +1,72 @@
 package Flicking;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
 public class Stone extends JLabel {
+	private final int FPS = 60;
+	private final int ONE_SEC = 1000;
+	private final int DELAY = ONE_SEC / FPS;
+	private int moveCnt;
+	private int stonePower;
+	private int moveSize;
+	private int moveDirection;
 	private ImageIcon icon;
-	
-	Stone(String path){
+	private Timer timer;
+
+	Stone(String path, Point init_point, String direction) {
 		icon = new ImageIcon(path);
 		setSize(icon.getIconWidth(), icon.getIconHeight());
 		setIcon(icon);
+
+		setLocation(init_point);
+		setBounds(getX() - getWidth() / 2, getY() - getHeight() / 2, getWidth(), getHeight());
+		setStoneMovement(direction);
 	}
+
+	private void setStoneMovement(String direction) {
+		stonePower = 100;
+		moveCnt = 0;
+		moveSize = 15;
+		if (direction.equalsIgnoreCase("up"))
+			moveDirection = -1;
+		else if (direction.equalsIgnoreCase("down"))
+			moveDirection = 1;
+
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				timer.start();
+			}
+		});
+
+		timer = new Timer(DELAY, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				moveCnt++;
+				moveStone(0, moveDirection * moveSize);
+				moveSize --;
+				if (moveCnt == 15) {
+					timer.stop();
+					moveCnt = 0;
+					moveSize = 15;
+				}
+			}
+		});
+	}
+
+	int getStonePower() {
+		return stonePower;
+	}
+
+	void moveStone(int xSize, int ySize) {
+		setLocation(getX() + xSize, getY() + ySize);
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
+
 }
